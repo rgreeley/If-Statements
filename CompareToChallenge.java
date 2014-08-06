9fbb3151b83b039c3b67dd47645c4e30f2588729
Robert Greeley <rgreeley@zappos.com>
2014-08-05 21:52:52 -0700

    Compare To Challenge EX38

---
+++ CompareToChallenge.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class CompareToChallenge
+{
+		public static void main( String[] args )
+		{
+			Scanner keyboard = new Scanner(System.in);
+
+				System.out.print("Comparing \"Antman\" with \"Atom\" ");
+				System.out.println("Antman".compareTo("Atom") );
+				
+				System.out.print("Comparing \"Hulk\" with \"Superman\" ");
+				System.out.println("Hulk".compareTo("Ironman") );
+				
+				System.out.print("Comparing \"Aquaman\" with \"Namor\" ");
+				System.out.println("Aquaman".compareTo("Namor") );
+				
+				System.out.print("Comparing \"Flash\" with \"Quicksilver\" ");
+				System.out.println("Flash".compareTo("Quicksilver") );
+				
+				System.out.print("Comparing \"Arrow\" with \"Hawkeye\" ");
+				System.out.println("Arrow".compareTo("Hawkeye") );
+				
+				System.out.print("Comparing \"Wolverine\" with \"Shazzam\" ");
+				System.out.println("Wolverine".compareTo("Shazzam") );
+				
+				System.out.print("Comparing \"Punisher\" with \"Batman\" ");
+				System.out.println("Punisher".compareTo("Batman") );
+				
+				System.out.print("Comparing \"WonderWoman\" with \"MsMarvel\" ");
+				System.out.println("WonderWoman".compareTo("MsMarvel") );
+				
+				System.out.print("Comparing \"Thor\" with \"GreenLanturn\" ");
+				System.out.println("Thor".compareTo("GreenLanturn") );
+				
+				System.out.print("Comparing \"JusticeLeague\" with \"Avengers\" ");
+				System.out.println("JusticeLeague".compareTo("Avengers") );
+				
+				System.out.print("Comparing \"Marvel\" with \"Marvel\" ");
+				System.out.println("Marvel".compareTo("Marvel") );
+				
+				System.out.print("Comparing \"DC\" with \"DC\" ");
+				System.out.println("DC".compareTo("DC") );
+
+		}
+}
+
+
+
